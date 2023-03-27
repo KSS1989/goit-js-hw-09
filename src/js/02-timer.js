b38c5da01a6f76e10a97cb5e9db7698518d86cd0
Serhii Klokun <99805859+KSS1989@users.noreply.github.com>
2023-03-27 23:32:58 +0300

    promis

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,12 +2,14 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+
 const dateTimePicker = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
 const days = document.querySelector('[data-days]');
 const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
+console.log(seconds);
 
 let selectedDate = null;
 let intervalId = null;
